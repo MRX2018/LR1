8389f906296d65853c29e843807029ee88affcc1
MRX2018 <59194011+MRX2018@users.noreply.github.com>
2021-06-03 15:00:53 +0500

    лр3
    лр3

---
+++ rss.php
@@ -0,0 +1,16 @@
+﻿<?php
+   $tek=$_GET["tek"];
+   $rss = simplexml_load_file("https://www.9tv.co.il/rss/mainfeed");      
+   $s=0;
+   foreach ($rss->channel->item as $item) {
+       $s++;
+       echo "<div class='news'>";
+       echo "<h3>".$item->title."</h3>";
+
+       echo '<p>'.$item->description.'</p>'; 
+       echo "<img src=\"". (string)$item->enclosure['url'][0]. "\"></div><br>";
+       if($s==$tek) {
+          break;
+         }
+   }
+?>
